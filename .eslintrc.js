6b2873020aa80c36a32140b1ba25a18b153fec28
yoyo930021 <yoyo930021@gamil.com>
2017-03-20 20:10:09 +0800

    modify eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,29 @@
-// http://eslint.org/docs/user-guide/configuring
-
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
+  parser: 'Espree',
   parserOptions: {
     sourceType: 'module'
   },
-  env: {
-    browser: true,
-  },
-  extends: 'airbnb-base',
+  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+  extends: 'standard',
   // required to lint *.vue files
   plugins: [
     'html'
   ],
-  // check if imports actually resolve
-  'settings': {
-    'import/resolver': {
-      'webpack': {
-        'config': 'build/webpack.base.conf.js'
-      }
-    }
-  },
   // add your custom rules here
   'rules': {
-    // don't require .vue extension when importing
-    'import/extensions': ['error', 'always', {
-      'js': 'never',
-      'vue': 'never'
-    }],
-    'no-console': 0,
-    // allow optionalDependencies
-    'import/no-extraneous-dependencies': ['error', {
-      'optionalDependencies': ['test/unit/index.js']
-    }],
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    'space-before-function-paren': ['error', 'never'],
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-  }
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    // disable handle-callback-err
+    'handle-callback-err': "off"
+  },
+  "env": {
+    "browser": true,
+    "es6": true,
+  },
 }
