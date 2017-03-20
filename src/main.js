d6c52df4a9d100bea3a10ee7a5031049758c5137
yoyo930021 <yoyo930021@gamil.com>
2017-03-20 21:10:18 +0800

    ui framwork vue-material to muse-ui

--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import VueMaterial from 'vue-material'
 import App from './App.vue'
 import store from './store/index'
 import router from './router/index'
 import { sync } from 'vuex-router-sync'
 import Components from './components/_index'
+import MuseUI from 'muse-ui'
+import 'muse-ui/dist/muse-ui.css'
 
 sync(store, router)
 
-Vue.use(VueMaterial);
+Vue.use(MuseUI)
 
 Object.keys(Components).forEach(key => {
   Vue.component(key, Components[key])
