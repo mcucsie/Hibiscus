3a3955d1078fc981be503165ed91a97a0c6959b0
yoyo930021 <yoyo930021@gamil.com>
2017-03-21 14:30:55 +0800

    add divider to comment

--- src/views/Moodle.vue
+++ src/views/Moodle.vue
@@ -3,19 +3,22 @@
     <mobile-tear-sheet>
       <mu-list>
         <mu-sub-header inset>Moodle 課程列表</mu-sub-header>
-        <mu-list-item v-for="update in updates" :title="update.name"
-                      :describeText="update.activity+' activities'" @click.native="openLink(update.link)">
-          <mu-avatar v-if="update.activity>0" 
-                     icon="assignment"
-                     backgroundColor="blue"
-                     slot="leftAvatar" />
-          <mu-avatar v-else
-                     icon="insert_chart"
-                     backgroundColor="yellow600"
-                     slot="leftAvatar" />
-          <mu-icon value="info"
-                   slot="right" />
-        </mu-list-item>
+        <template v-for="(update,index) in updates">
+          <mu-list-item :title="update.name"
+                        :describeText="update.activity+' activities'" @click.native="openLink(update.link)">
+            <mu-avatar v-if="update.activity>0" 
+                      icon="assignment"
+                      backgroundColor="blue"
+                      slot="leftAvatar" />
+            <mu-avatar v-else
+                      icon="insert_chart"
+                      backgroundColor="yellow600"
+                      slot="leftAvatar" />
+            <mu-icon value="info"
+                    slot="right" />
+          </mu-list-item>
+          <!--<mu-divider v-if="(index+1)!=updates.length" />-->
+        </template>
       </mu-list>
     </mobile-tear-sheet>
   </div>
